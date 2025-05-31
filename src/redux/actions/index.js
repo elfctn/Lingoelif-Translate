a76c82bd86bff3a673060059324a1d2a240f01cf
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 22:04:01 +0300

    actions done -reduxtoolkit

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,11 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { api } from './../../utils/api';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { api } from "./../../utils/api";
 
 // api'dan dil verilerini alıp store'a dispatch edicek asenkron thunk askiyonu
 export const getLanguages = createAsyncThunk(
-  'language/getLanguages',
+  "language/getLanguages",
   async () => {
-    const res = await api.get('/getLanguages');
+    const res = await api.get("/getLanguages");
 
     return res.data.data.languages;
   }
@@ -13,21 +13,21 @@ export const getLanguages = createAsyncThunk(
 
 // apidan çeviri sonucunu alır
 export const translateText = createAsyncThunk(
-  'translate/translateText',
+  "translate/translateText",
   async (p) => {
     // api'a gönderilcek parametreleri belirle
     const params = new URLSearchParams();
-    params.set('source_language', p.sourceLang.value);
-    params.set('target_language', p.targetLang.value);
-    params.set('text', p.text);
+    params.set("source_language", p.sourceLang.value);
+    params.set("target_language", p.targetLang.value);
+    params.set("text", p.text);
 
     // api'a gönderilcek header'ı belirle
     const headers = {
-      'content-type': 'application/x-www-form-urlencoded',
+      "content-type": "application/x-www-form-urlencoded",
     };
 
     // api isteği at
-    const res = await api.post('/translate', params, { headers });
+    const res = await api.post("/translate", params, { headers });
 
     // aksiyonun payload'ını return ediyoruz
     return res.data.data.translatedText;
