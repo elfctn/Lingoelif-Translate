843ba3548bbafde234e57cbb5ecdbb3fd07249e6
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 22:01:16 +0300

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { getLanguages, translateText } from "./redux/actions";
 import Select from "react-select";
 import { setAnswer } from "./redux/slices/translateSlice";
 import React from "react";
+
 const App = () => {
   const langState = useSelector((store) => store.language);
   const translateState = useSelector((store) => store.translate);
@@ -24,8 +25,6 @@ const App = () => {
     dispatch(getLanguages());
   }, []);
 
-  // apidan gelen diziyi bizden sitenilen formata çevir
-  // nesnelerin code ve name değerilerini label ve value'ya çevirdik
   const formatted = useMemo(
     () =>
       langState.languages?.map((i) => ({
@@ -40,82 +39,84 @@ const App = () => {
   };
 
   const handleSwap = () => {
-    // select alanlarındaki veirleri yer değiş
     setSourceLang(targetLang);
     setTargetLang(sourceLang);
-
-    // store'daki veriyi state'e aktar
     setText(translateState.answer);
-
-    // state'deki veriyi store'a  aktar
     dispatch(setAnswer(text));
   };
 
   return (
-    <div className="bg-white h-screen text-black grid place-items-center">
-      <div className="w-[80vw] max-w-[1100px] flex flex-col justify-center">
-        <h1 className="text-center text-6xl font-semibold mb-7 text-rose-600 flex items-center justify-center gap-4">
-          LingoElif{" "}
-          <img src="/lingoelif.svg" alt="app_icon" className="w-16 h-16" />
-        </h1>
+    <>
+      <div className="bg-white h-screen text-black grid place-items-center">
+        <div className="w-[80vw] max-w-[1100px] flex flex-col justify-center">
+          <h1 className="text-center text-6xl font-semibold mb-7 text-rose-600 flex items-center justify-center gap-4">
+            LingoElif{" "}
+            <img src="/lingoelif.svg" alt="app_icon" className="w-16 h-16" />
+          </h1>
 
-        <div className="flex gap-2">
-          <Select
-            onChange={(lang) => setSourceLang(lang)}
-            value={sourceLang}
-            isDisabled={langState.isLoading}
-            isLoading={langState.isLoading}
-            options={formatted}
-            className="flex-1"
-          />
-
-          <button
-            onClick={handleSwap}
-            className="rounded py-2 px-6 bg-rose-100 text-black transition hover:ring-2 hover:bg-rose-200"
-          >
-            Değiş
-          </button>
-
-          <Select
-            onChange={(lang) => setTargetLang(lang)}
-            value={targetLang}
-            isDisabled={langState.isLoading}
-            isLoading={langState.isLoading}
-            options={formatted}
-            className="flex-1"
-          />
-        </div>
-
-        <div className="flex mt-5 gap-3 md:gap-[105px] max-md:flex-col">
-          <textarea
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded border border-rose-200 focus:ring-2 focus:ring-rose-400"
-          />
-
-          <div className="w-full relative">
-            <textarea
-              disabled
-              className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded border border-rose-200 text-gray-700 bg-gray-100"
-              value={translateState.answer}
+          <div className="flex gap-2">
+            <Select
+              onChange={(lang) => setSourceLang(lang)}
+              value={sourceLang}
+              isDisabled={langState.isLoading}
+              isLoading={langState.isLoading}
+              options={formatted}
+              className="flex-1"
             />
 
-            {translateState.isLoading && (
-              <h1 className="absolute top-[50%] left-[50%] translate-x-[-50%] text-rose-500">
-                Yükleniyor...
-              </h1>
-            )}
-          </div>
-        </div>
+            <button
+              onClick={handleSwap}
+              className="rounded py-2 px-6 bg-rose-100 text-black transition hover:ring-2 hover:bg-rose-200"
+            >
+              Değiş
+            </button>
 
-        <button
-          onClick={handleTranslate}
-          className="rounded-md py-3 px-5 text-[17px] font-semibold cursor-pointer bg-rose-500 text-white mt-3 hover:ring-2 hover:bg-rose-600 transition"
-        >
-          Çevir
-        </button>
+            <Select
+              onChange={(lang) => setTargetLang(lang)}
+              value={targetLang}
+              isDisabled={langState.isLoading}
+              isLoading={langState.isLoading}
+              options={formatted}
+              className="flex-1"
+            />
+          </div>
+
+          <div className="flex mt-5 gap-3 md:gap-[105px] max-md:flex-col">
+            <textarea
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded border border-rose-200 focus:ring-2 focus:ring-rose-400"
+            />
+
+            <div className="w-full relative">
+              <textarea
+                disabled
+                className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded border border-rose-200 text-gray-700 bg-gray-100"
+                value={translateState.answer}
+              />
+
+              {translateState.isLoading && (
+                <h1 className="absolute top-[50%] left-[50%] translate-x-[-50%] text-rose-500">
+                  Yükleniyor...
+                </h1>
+              )}
+            </div>
+          </div>
+
+          <button
+            onClick={handleTranslate}
+            className="rounded-md py-3 px-5 text-[17px] font-semibold cursor-pointer bg-rose-500 text-white mt-3 hover:ring-2 hover:bg-rose-600 transition"
+          >
+            Çevir
+          </button>
+        </div>
       </div>
-    </div>
+
+      <footer className="w-full text-center text-gray-400 text-sm py-2 fixed bottom-0 left-0 select-none pointer-events-none">
+        2025 - ELIFCETIN
+      </footer>
+    </>
   );
 };
+
 export default App;
